70bed549fef2d685f218bb635645a0495eded049
Max Kingston <37544026+Max4960@users.noreply.github.com>
2024-04-23 21:48:32 +0100

    Code refactoring with switch statements

--- coursework/src/main/java/uk/ac/soton/comp1206/scene/LobbyScene.java
+++ coursework/src/main/java/uk/ac/soton/comp1206/scene/LobbyScene.java
@@ -7,7 +7,9 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -29,6 +31,7 @@ public class LobbyScene extends BaseScene {
     final Communicator communicator;
     private static final Logger logger = LogManager.getLogger(LobbyScene.class);
     private StackPane lobbyPane = new StackPane();
+    private boolean inLobby = false;
     VBox lobbyBox = new VBox();
 
     SimpleListProperty<String> lobbyNames;
@@ -49,16 +52,27 @@ public class LobbyScene extends BaseScene {
     public void initialise() {
         // Checking lobbies every 5 seconds
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-            communicator.send("LIST");
-            communicator.addListener(this::fetchList);
-            Platform.runLater(() -> {
-                loadLobbies();
-            });
+            if (!inLobby) {
+                communicator.send("LIST");
+                //communicator.addListener(this::fetchList);
+                //Platform.runLater(() -> {
+                //    loadLobbies();
+                //});
+            }
+
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
 
+        communicator.addListener(event -> {
+            Platform.runLater(() -> {
+                receiver(event);
+            });
+        });
+
     }
+
+
 
     private void fetchList(String message) {
         //logger.info(message);
@@ -71,6 +85,42 @@ public class LobbyScene extends BaseScene {
             lobbyList.add(lobby);
         }
         lobbyNames.set(FXCollections.observableList(lobbyList));
+    }
+
+    private void receiver(String message) {
+        logger.info("Receiver received " + message);
+        String[] parts = message.split(" ");
+        String command = parts[0];
+        String info = parts[1];
+
+        logger.info(command);
+        logger.info(info);
+
+        switch (command) {
+            case "JOIN":
+                break;
+            case "HOST":
+                break;
+            case "CHANNELS":
+                fetchList(message);
+                loadLobbies();
+                logger.info(command + " " + info);
+                break;
+            case "NICK":
+                break;
+            case "START":
+                break;
+            case "PARTED":
+                break;
+            case "USERS":
+                break;
+            case "MSG":
+                break;
+            case "ERROR":
+                break;
+            case "QUIT":
+                break;
+        }
     }
 
 
@@ -88,7 +138,38 @@ public class LobbyScene extends BaseScene {
         Text lobbyText = new Text("Active Lobbies:");
         lobbyText.getStyleClass().add("heading");
         lobbyBox.getChildren().add(lobbyText);
+        lobbyListViewer.setMaxWidth(0.4*gameWindow.getWidth());
+        // *Graphics design is my passion*
+        lobbyListViewer.setStyle("-fx-background-color: black; -fx-opacity: 0.8; -fx-border-color: red;");
         lobbyBox.getChildren().add(lobbyListViewer);
+
+        TextField createLobbyName = new TextField();
+        createLobbyName.setMaxWidth(0.4*gameWindow.getWidth());
+        createLobbyName.setPromptText("Enter a lobby name...");
+        lobbyBox.getChildren().addAll(createLobbyName);
+
+        Button createLobbyButton = new Button("Create Lobby");
+        createLobbyButton.setStyle("-fx-background-color: red; -fx-text-fill: white");
+        lobbyBox.getChildren().add(createLobbyButton);
+
+        createLobbyButton.setOnAction(event -> {
+            createLobby(createLobbyName.getText());
+        });
+
+        lobbyListViewer.setOnMouseClicked(event -> {
+            String selectedLobby = lobbyListViewer.getSelectionModel().getSelectedItem();
+            joinLobby(selectedLobby);
+        });
+    }
+
+    public void createLobby(String lobbyName) {
+        communicator.send("CREATE " + lobbyName);
+    }
+
+    public void joinLobby(String lobby) {
+        inLobby = true;
+        logger.info("Joining: " + lobby);
+        communicator.send("JOIN " + lobby);
     }
 
     public void loadLobbies() {
